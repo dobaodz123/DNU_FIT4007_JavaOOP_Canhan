45db415f7e6c9871710e731404d1277a7f9a1569
dobaodz123 <dongbao0606@gmail.com>
2025-10-07 20:17:21 +0700

    Add files via upload

---
+++ vonglap2/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên dương N : ");
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
