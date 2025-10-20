a2d3a72ccaa055d9f8bee06c8c630a9235c082f0
dobaodz123 <dongbao0606@gmail.com>
2025-10-20 23:02:56 +0700

    Add files via upload

---
+++ chuoi8/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số dương N :");
+        int n = sc.nextInt();
+        String s ="";
+        for(int i=1 ;i<=n ;i++){
+            s+="+";
+            System.out.println(s);
+        }
+    }
+}
